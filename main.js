a761b0b6f6dfd45fbb4c5f8fb285f6cb32fde414
fritz Woll <fritzwoll@fritzs-MacBook.local>
2019-10-18 17:51:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var individualQuotes = [];
 var arr = new Array(20)
-
+var lastIndex = 0;
 
 $.ajax({
   type: 'GET',
@@ -37,18 +37,20 @@ function randomImage() {
   var theImage = document.getElementById('myimage');
   var imgDir = 'bruce_pics/';
   var imgIndex = 0;
-  var lastIndex = 0;
+
 
   if(arr.length > 1) {
     while(arr == lastIndex) {
       arr = Math.floor(Math.random())
     }
+
     lastIndex = imgIndex;
 
     var imgPath = imgDir + arr[imgIndex];
 
     theImage.src = imgPath;
     theImage.alt = arr[imgIndex];
+
   } else {
     return false;
   }
