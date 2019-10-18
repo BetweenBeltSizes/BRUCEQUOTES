dd09281d5bc6090c8fe25725041eeeb7a9394582
fritz Woll <fritzwoll@fritzs-MacBook.local>
2019-10-18 17:42:06 +0200

    pics and update js

--- main.js
+++ main.js
@@ -1,22 +1,55 @@
 var individualQuotes = [];
+var arr = new Array(20)
+
 
 $.ajax({
   type: 'GET',
-  url: '/myfile.txt',
+  url: 'https://raw.githubusercontent.com/BetweenBeltSizes/BRUCEQUOTES/master/the_quotes/myfile.txt',
   async: false
 }).done(function (data) {
-  console.log(data);
+
   individualQuotes = data.split('\n');
 });
 
+$('.get-quote-btn').on('click', function(){
+  event.preventDefault();
+  getNewQuote();
+  randomImage();
+})
+
 getNewQuote();
+makeArray();
+randomImage();
 
 function getNewQuote() {
     var random = Math.floor(Math.random() * individualQuotes.length);
     $('#quote').text(individualQuotes[random]);
 }
 
-$('.get-quote-btn').on('click', function(){
-  event.preventDefault();
-  getNewQuote();
-})
+function makeArray() {
+      for(let i=0; i<arr.length;i++){
+          arr[i] = i + ".img";
+          console.log(arr[i]);
+      }
+}
+
+function randomImage() {
+  var theImage = document.getElementById('myimage');
+  var imgDir = 'bruce_pics/';
+  var imgIndex = 0;
+  var lastIndex = 0;
+
+  if(arr.length > 1) {
+    while(arr == lastIndex) {
+      arr = Math.floor(Math.random())
+    }
+    lastIndex = imgIndex;
+
+    var imgPath = imgDir + arr[imgIndex];
+
+    theImage.src = imgPath;
+    theImage.alt = arr[imgIndex];
+  } else {
+    return false;
+  }
+}
