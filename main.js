c901c181d0e7ea1040ad73d193515ff3783f911a
fritz Woll <fritzwoll@fritzs-MacBook.local>
2019-10-21 17:08:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ $.ajax({
   type: 'GET',
   url: 'https://raw.githubusercontent.com/BetweenBeltSizes/BRUCEQUOTES/master/the_quotes/myfile.txt',
   async: false
-}).done(function (data) {
+}).done(function(data) {
 
   individualQuotes = data.split('\n');
 });
 
-$('.get-quote-btn').on('click', function(){
+$('.get-quote-btn').on('click', function() {
   event.preventDefault();
   getNewQuote();
   randomImage();
@@ -23,44 +23,32 @@ makeArray();
 randomImage();
 
 function getNewQuote() {
-    var random = Math.floor(Math.random() * individualQuotes.length);
-    $('#quote').text(individualQuotes[random]);
+  var random = Math.floor(Math.random() * individualQuotes.length);
+  $('#quote').text(individualQuotes[random]);
 }
 
 function makeArray() {
-      for(let i=0; i<arr.length;i++){
-          arr[i] = i + ".jpg";
-          console.log(arr[i]);
-      }
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = i + ".jpg";
+    console.log(arr[i]);
+  }
 }
 
 function randomImage() {
   var theImage = document.getElementById('myimage');
   var imgDir = 'bruce_pics/';
-  var imgIndex = 0;
 
+  var imgPath = imgDir + arr[Math.floor(Math.random() * arr.length)];
 
-  if(arr.length > 1) {
-    while(imgIndex == lastIndex) {
-      imgIndex = Math.floor(Math.random() * arr.length);
-    }
+  theImage.src = imgPath;
 
-    lastIndex = imgIndex;
-
-    var imgPath = imgDir + arr[imgIndex];
-
-    theImage.src = imgPath;
-    theImage.alt = arr[imgIndex];
-
-  } else {
-    return false;
-  }
 }
 
 
 function Image() {
   var x = document.createElement("IMG");
   x.setAttribute("src", "bruce_pics/1.jpg");
+  x.style.left = "0px";
   x.setAttribute("width", "304");
   x.setAttribute("height", "228");
   x.setAttribute("alt", "The Pulpit Rock");
